d4bef7e919129c67a794d487947b260ec56610f1
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-07-31 21:23:00 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -8,18 +8,9 @@ app.secret_key = 'your_secret_key'  # Replace with your own secret key
 
 # MongoDB connection setup
 client = MongoClient('mongodb+srv://tiak:mongodb.ak17@portfolio-dataset.ha4l0ka.mongodb.net/')
-# db_blogs = client['blogs']
-# blogs_collection = db_blogs['blogs']
-
-# db_projects = client['projects']
-# projects_collection = db_projects['test']
-
-# db_certificates = client['certificates']
-# certificates_collection = db_certificates['certificates']
 
 db_users = client['users']
 users_collection = db_users['users']
-
 
 # Route to render the main page
 @app.route('/')
@@ -29,51 +20,72 @@ def login():
 # Route for main page to get database and collection names
 @app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
-    if request.method == 'POST' and 'username' in session:
+    if request.method == 'POST':
         db_name = request.form.get('db_name')
         collection_name = request.form.get('collection_name')
         session['db_name'] = db_name
         session['collection_name'] = collection_name
-        return redirect(url_for('list_blogs'))
+        return redirect(url_for('list_documents', db_name=db_name))
     return render_template('dashboard.html')
 
-# Route for displaying blogs based on selected DB and Collection
-@app.route('/documents')
-def list_blogs():
-    db_name = session.get('db_name')
+# Route for displaying documents based on selected DB and Collection
+@app.route('/<db_name>')
+def list_documents(db_name):
     collection_name = session.get('collection_name')
 
-    if db_name and collection_name and 'username' in session:
+    if collection_name and 'username' in session:
         db = client[db_name]
         collection = db[collection_name]
         dataset = collection.find()
-        return render_template('index.html', dataset=dataset)
+        return render_template('index.html', dataset=dataset, db_name=db_name)
     else:
         return redirect(url_for('dashboard'))
 
-# Route for creating a new blog post
+# Route for creating a new document (blog, project, certificate)
 @app.route('/create', methods=['GET', 'POST'])
 def create():
-    if request.method == 'POST' and 'username' in session :
-        date_str = request.form.get('date')
-        date_string = request.form.get('dateString')
+    if request.method == 'POST' and 'username' in session:
+        db_name = session.get('db_name')
+        collection_name = session.get('collection_name')
 
-        data = {
-            'title': request.form.get('title').replace('\r', ''),
-            'author': request.form.get('author').replace('\r', ''),
-            'imageurl': request.form.get('imageurl').replace('\r', ''),
-            'description': request.form.get('description').replace('\r', ''),
-            'content': request.form.get('content').replace('\r', ''),
-            'date': date_str.replace('\r', ''),
-            'dateString': date_string.replace('\r', ''),
-            'category': request.form.get('category').replace('\r', ''),
-            'pinned': request.form.get('pinned') == 'true'
-        }
-        blogs_collection.insert_one(data)
-        return redirect(url_for('list_blogs'))
+        if db_name and collection_name:
+            db = client[db_name]
+            collection = db[collection_name]
+
+            if collection_name == 'blogs':
+                data = {
+                    'title': request.form.get('title').replace('\r', ''),
+                    'author': request.form.get('author').replace('\r', ''),
+                    'imageurl': request.form.get('imageurl').replace('\r', ''),
+                    'description': request.form.get('description').replace('\r', ''),
+                    'content': request.form.get('content').replace('\r', ''),
+                    'date': request.form.get('date').replace('\r', ''),
+                    'dateString': request.form.get('dateString').replace('\r', ''),
+                    'category': request.form.get('category').replace('\r', ''),
+                    'pinned': request.form.get('pinned') == 'true'
+                }
+            elif collection_name == 'projects':
+                data = {
+                    'imageUrl': request.form.get('imageUrl').replace('\r', ''),
+                    'title': request.form.get('title').replace('\r', ''),
+                    'date': request.form.get('date').replace('\r', ''),
+                    'description': request.form.get('description').replace('\r', ''),
+                    'link': request.form.get('link').replace('\r', ''),
+                    'dateString': request.form.get('dateString').replace('\r', '')
+                }
+            elif collection_name == 'certificates':
+                data = {
+                    'date': request.form.get('date').replace('\r', ''),
+                    'description': request.form.get('description').replace('\r', ''),
+                    'imageUrl': request.form.get('imageUrl').replace('\r', ''),
+                    'title': request.form.get('title').replace('\r', '')
+                }
+
+            collection.insert_one(data)
+            return redirect(url_for('list_documents', db_name=db_name))
     return render_template('create.html')
 
-# Route for editing a blog post
+# Route for editing a document (blog, project, certificate)
 @app.route('/edit/<id>', methods=['GET', 'POST'])
 def edit(id):
     if 'username' in session:
@@ -86,36 +98,61 @@ def edit(id):
             return redirect(url_for('dashboard'))
 
         try:
-            blog_id = ObjectId(id)
-            blog = collection.find_one({'_id': blog_id})
+            doc_id = ObjectId(id)
+            document = collection.find_one({'_id': doc_id})
+            if not document:
+                document = collection.find_one({'_id': id})
         except Exception as e:
-            print(f"Error fetching blog: {e}")
-            return redirect(url_for('list_blogs'))
+            print(f"Error fetching document: {e}")
+            return redirect(url_for('list_documents', db_name=db_name))
 
         if request.method == 'POST':
-            updated_data = {
-                'title': request.form.get('title').replace('\r', ''),
-                'author': request.form.get('author').replace('\r', ''),
-                'imageurl': request.form.get('imageurl').replace('\r', ''),
-                'description': request.form.get('description').replace('\r', ''),
-                'content': request.form.get('content').replace('\r', ''),
-                'date': request.form.get('date').replace('\r', ''),
-                'dateString': request.form.get('dateString').replace('\r', ''),
-                'category': request.form.get('category').replace('\r', ''),
-                'pinned': request.form.get('pinned') == 'true'
-            }
-            collection.update_one({'_id': blog_id}, {'$set': updated_data})
-            return redirect(url_for('list_blogs'))
+            if collection_name == 'blogs':
+                updated_data = {
+                    'title': request.form.get('title').replace('\r', ''),
+                    'author': request.form.get('author').replace('\r', ''),
+                    'imageurl': request.form.get('imageurl').replace('\r', ''),
+                    'description': request.form.get('description').replace('\r', ''),
+                    'content': request.form.get('content').replace('\r', ''),
+                    'date': request.form.get('date').replace('\r', ''),
+                    'dateString': request.form.get('dateString').replace('\r', ''),
+                    'category': request.form.get('category').replace('\r', ''),
+                    'pinned': request.form.get('pinned') == 'true'
+                }
+            elif collection_name == 'projects':
+                updated_data = {
+                    'imageUrl': request.form.get('imageUrl').replace('\r', ''),
+                    'title': request.form.get('title').replace('\r', ''),
+                    'date': request.form.get('date').replace('\r', ''),
+                    'description': request.form.get('description').replace('\r', ''),
+                    'link': request.form.get('link').replace('\r', ''),
+                    'dateString': request.form.get('dateString').replace('\r', '')
+                }
+            elif collection_name == 'certificates':
+                updated_data = {
+                    'date': request.form.get('date').replace('\r', ''),
+                    'description': request.form.get('description').replace('\r', ''),
+                    'imageUrl': request.form.get('imageUrl').replace('\r', ''),
+                    'title': request.form.get('title').replace('\r', '')
+                }
 
-        return render_template('edit.html', blog=blog)
+            collection.update_one({'_id': doc_id}, {'$set': updated_data})
+            return redirect(url_for('list_documents', db_name=db_name))
+
+        return render_template('edit.html', document=document, db_name=db_name)
     else:
         return render_template('login.html')
 
-# Route for deleting a blog post
+# Route for deleting a document (blog, project, certificate)
 @app.route('/delete/<id>', methods=['POST'])
 def delete(id):
-    blogs_collection.delete_one({'_id': ObjectId(id)})
-    return redirect(url_for('list_blogs'))
+    db_name = session.get('db_name')
+    collection_name = session.get('collection_name')
+    if db_name and collection_name:
+        db = client[db_name]
+        collection = db[collection_name]
+        collection.delete_one({'_id': ObjectId(id)})
+    return redirect(url_for('list_documents', db_name=db_name))
 
 # Route to verify login credentials
 @app.route('/api/login', methods=['POST'])
@@ -126,7 +163,7 @@ def api_login():
 
     user = users_collection.find_one({'username': username})
 
-    if user and check_password_hash(user['password'], password) and 'username' not in session:
+    if user and check_password_hash(user['password'], password):
         session['username'] = username
         return jsonify({'success': True, "message": "success"})
     else:
